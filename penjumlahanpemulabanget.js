473d40134d0fd51c640f7cd933780f1875d0d297
DERMANRAY <rizkydermawan12345@gmail.com>
2025-01-05 18:11:12 +0700

    Add files via upload

---
+++ penjumlahanpemulabanget.js
@@ -0,0 +1,11 @@
+/**
+ * buat penjumlahan 3 inputan yang sederhana banget
+ */
+
+const angkapertama = 5
+const angkakedua = 5
+const angkaketiga = 5
+
+const sum = angkapertama + angkakedua + angkaketiga
+
+console.log(`hasil penjumlahan dari\n${angkapertama} + ${angkakedua} + ${angkaketiga}\n= ${sum} `)
